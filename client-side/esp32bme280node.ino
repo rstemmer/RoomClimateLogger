38fb37fbd0dd82792d57fb551ecb82f33645ee26
Ralf Stemmer <ralf.stemmer@gmx.net>
2020-08-14 14:39:47 +0200

    Access sensor

--- client-side/esp32bme280node.ino
+++ client-side/esp32bme280node.ino
@@ -1,6 +1,9 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 const char *ssid     = "SSID";                      // TODO: Insert your WiFi's SSID here
 const char *password = "PASSWORD";                  // TODO: Insert your WiFi's access password
@@ -11,44 +14,143 @@ IPAddress myipaddr(192,168,1,51);                   // \.
 IPAddress gateway(192,168,1,1);                     //  > TODO: Adapt to your network setup
 IPAddress subnet(255,255,255,0);                    // /
 
+      unsigned long deadline;
+const unsigned long interval = 60000; // send data every 60 seconds
+
+Adafruit_BME280 bme;
+
+
+
+void WiFiConnect()
+{
+    do
+    {
+        // Try to connect
+        WiFi.begin(ssid, password);
+
+        // Wait 10 seconds for the connection
+        for(int i = 0; i < 20 and WiFi.status() != WL_CONNECTED; i++)
+            delay(500);
+
+        // If still not connected, try to connect again
+    }
+    while(WiFi.status() != WL_CONNECTED);
+    return;
+}
+
+
+// Endless loop letting an LED blink with 1Hz
+[[ noreturn ]] void GraveLight()
+{
+    // My ESP32 has only an UART activity LED.
+    // So I spam data to the UART to make this LED flash
+    const unsigned long interval = 500;
+    unsigned long toggletime;
+
+    // This is a post-mortem function.
+    // I do not care about real-time and potential integer overflows.    
+    while(true)
+    {
+        // LED on
+        toggletime = millis() + interval;
+        do
+        {
+            Serial.print("✝");
+            delay(10); // Give the LED an eerie flicker.
+        }
+        while(millis() < toggletime);
+
+        // LED off
+        delay(500);
+    }
+}
+
+
+
 void setup()
 {
+    // Setup UART
     Serial.begin(9600);
     while(!Serial);
+
+    // Setup WiFi
+    if(WiFi.status() == WL_NO_SHIELD)
+    {
+        Serial.println("No WiFi hardware!");
+        GraveLight(); // no return
+    }
     
     auto success = WiFi.config(myipaddr, gateway, subnet);
     if(not success)
     {
         Serial.println("WiFi setup failed!");
-        while(1);
+        GraveLight(); // no return
     }
 
-    WiFi.begin(ssid, password);
-    while(WiFi.status() != WL_CONNECTED)
+    WiFiConnect();
+
+    // Setup Sensors
+    bool sensorready;
+    sensorready = bme.begin();
+    if(not sensorready)
     {
-        delay(500);
+        Serial.println("Sensor setup failed!");
+        GraveLight(); // no return
     }
 
+    // Initialize deadline for collecting samples
+    deadline = millis() + interval; // deadline for data
 }
+
+
 
 void loop()
 {
+    // Check WiFi status.
+    // In case this node got disconnected, it should try to reconnect.
     if(WiFi.status() != WL_CONNECTED)
     {
         Serial.println("Disconnected!");
-        delay(1000);
+
+        // Try to reconnect.
+        // This function is blocking until the connection succeeds.
+        WiFiConnect();
+
+        // Due to the connection issues we already lost lots of samples and data.
+        // Set new deadline for a fresh restart of logging.
+        deadline = millis() + interval;
         return;
     }
 
-    // Get Data
-    float t = 1.0;
-    float h = 2.0;
+    // Collect as many samples as possible until the deadline arrives.
+    // This loop consideres overflows of deadline and millis().
+    unsigned long timestamp;
+    constexpr const unsigned long msbmask = 0x80 << (sizeof(unsigned long) - 1);
+    float temp    = 0.0;
+    float hum     = 0.0;
+    int   samples = 0;
+    do
+    {
+        temp   += bme.readTemperature();
+        hum    += bme.readHumidity();
+        samples++;
+
+        delay(1000);  // Wait a second for the next sample     
+        timestamp = millis();
+    }
+    while(timestamp < deadline and (deadline^timestamp) & msbmask);
+    
+    // Calculate average temperature and humidity
+    //  observed during the last measurement interval
+    deadline += interval;
+    temp     /= samples;
+    hum      /= samples;
 
     // Prepare Data
     String postdata;
     postdata =  "r=" + String(roomname)
-             + "&t=" + String(t)
-             + "&h=" + String(h);
+             + "&t=" + String(temp)
+             + "&h=" + String(hum);
     
     // Send Data
     HTTPClient http;
@@ -59,7 +161,8 @@ void loop()
     retval = http.POST(postdata);
     http.end();
 
-    if(retval == 200)
+    // Check if sending the data succeeded
+    if(retval == HTTP_CODE_OK)
     {
         // Success!
     }
@@ -74,6 +177,6 @@ void loop()
         Serial.println(retval);
     }
 
-    delay(1000);
     return;
 }
+
